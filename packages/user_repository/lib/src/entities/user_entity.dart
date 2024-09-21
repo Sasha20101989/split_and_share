7ee90ea0103265c9b5e28da889df61010757b48d
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2024-09-21 21:05:50 +0300

    init

---
+++ packages/user_repository/lib/src/entities/user_entity.dart
@@ -0,0 +1,29 @@
+class MyUserEntity {
+  String userId;
+  String email;
+  String name;
+  bool hasActiveCard;
+
+  MyUserEntity(
+      {required this.userId,
+      required this.email,
+      required this.name,
+      required this.hasActiveCard});
+
+  Map<String, Object> toDocument() {
+    return {
+      'userId': userId,
+      'email': email,
+      'name': name,
+      'hasActiveCard': hasActiveCard
+    };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntity(
+        userId: doc['userId'],
+        email: doc['email'],
+        name: doc['name'],
+        hasActiveCard: doc['hasActiveCard']);
+  }
+}
