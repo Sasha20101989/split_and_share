c6ede56d779047ea69b863987a283656edf8b4f7
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2024-09-21 21:42:46 +0300

    add blocs

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
+
+import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
@@ -8,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider<AuthenticationBloc>(
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: MyAppView(),
+    );
   }
 }
