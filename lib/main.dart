5112d42ca56205edf12d67afb296655ee4515cd9
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2024-09-21 21:31:48 +0300

    add repo

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:split_and_share/app.dart';
 import 'package:split_and_share/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MyApp());
+  runApp(MyApp(FirebaseUserRepo()));
 }
